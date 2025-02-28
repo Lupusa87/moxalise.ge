d67e3648cbea75702780165fb9b73e82a5db59b8
Vakhtangi Abashidze <VakhtangiAbashidze@gmail.com>
2025-02-28 13:01:29 -0500

    Update z-index values and toggle button label

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -28,6 +28,16 @@ function formatValue(value) {
 
 // Function to toggle tabs panel
 function toggleTabs() {
+
+  const toogleTabs1 = document.getElementById('toogleTabs1');
+
+  if (toogleTabs1.innerHTML === "მონაცემების პანელი") {
+    toogleTabs1.innerHTML = 'ინტერაქტიული რუკა';
+  }
+  else {
+    toogleTabs1.innerHTML = 'მონაცემების პანელი';
+  }
+
   const panel = document.getElementById('tabs-panel');
   const map = document.getElementById('map');
   const isHidden = panel.style.display === 'none' || !panel.style.display;
